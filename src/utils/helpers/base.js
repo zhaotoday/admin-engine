0908ce46a92f1a5e5b0b59f62b0ed0576d608799
zhaojintian <729234283@qq.com>
2020-03-07 11:51:10 +0800

    优化

--- src/utils/helpers/base.js
+++ src/utils/helpers/base.js
@@ -1,6 +1,5 @@
 import consts from "@/utils/consts";
 import helpers from "jt-helpers";
-import dayjs from "dayjs";
 
 export default {
   ...helpers,
@@ -22,31 +21,5 @@ export default {
     return items && items.length
       ? items.find(item => item[key] === val) || {}
       : {};
-  },
-  getOption(options, value) {
-    return options.find(item => item.value === value) || {};
-  },
-  getOrderRoute({ alias = "orders", status, wxUserId }) {
-    const listSearchWhere = encodeURIComponent(
-      JSON.stringify({
-        no: { $like: "" },
-        payWay: { $eq: "" },
-        status: status ? { $eq: status } : "",
-        wxUserId: { $eq: wxUserId || "" },
-        startTime: {
-          $eq: dayjs()
-            .add(-1, "day")
-            .startOf("day")
-            .format("YYYY-MM-DD HH:mm:ss")
-        },
-        endTime: {
-          $eq: dayjs()
-            .endOf("day")
-            .format("YYYY-MM-DD HH:mm:ss")
-        }
-      })
-    );
-
-    return `/${alias}/orders/index?listSearchWhere=${listSearchWhere}`;
   }
 };
